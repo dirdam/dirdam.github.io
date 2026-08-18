5ec7d7ed5dd17885eb749f72fb8da2820b3ba99e
dirdam <adrian.jimenez@insightedge.jp>
2026-08-18 17:58:53 +0900

    Add favicon

--- dirdam.squadro.app/deploy.sh
+++ dirdam.squadro.app/deploy.sh
@@ -2,4 +2,5 @@
 
 echo "Deploying landing page to server..."
 scp index.html root@46.225.104.62:/var/www/dirdam.squadro.app/index.html
+scp favicon.ico root@46.225.104.62:/var/www/dirdam.squadro.app/favicon.ico
 echo "✓ Landing page deployed!"
